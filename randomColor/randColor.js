f6b62dc33cfac28f75074d1f451faefa63210edb
Madihander <79453431+Madihander@users.noreply.github.com>
2023-06-04 12:58:23 +0600

    Update randColor.js
    Теперь меняется также RGB

--- randomColor/randColor.js
+++ randomColor/randColor.js
@@ -4,18 +4,35 @@ const hex = [
 ];
 
 const changeBtn = document.getElementById("changeBtn")
-const color = document.getElementById("colorHex")
+const colorHex = document.getElementById("colorHex")
+let colorRGB = document.getElementById("colorRGB")
+let colorHSV = document.getElementById("colorHSV")
+let colorHSL = document.getElementById("colorHSL")
+
+
 const background = document.querySelector('.backgroundColor')
 changeBtn.addEventListener("click", () => {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
         hexColor += hex[getRandomNumber()]
     }
-    color.textContent = hexColor;
+    colorHex.textContent = hexColor;
     background.style.background = hexColor;
+    colorRGB.textContent = hexToRgb(hexColor);
 })
 
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length);
+}
+function hexToRgb(hex) {
+    // Remove the # character if present
+    hex = hex.replace("#", "");
+
+    // Convert the hex values to decimal
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+    // Return the RGB values as an object
+    return `${r}, ${g}, ${b}`;
 }
 
